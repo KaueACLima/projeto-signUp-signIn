c3d352c50ea372aa91dad8e3920ae84f68f22e2f
Kauê Lima <kaue5939.lima@gmail.com>
2024-02-20 20:37:40 -0300

    refactor:alter index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,3 @@
-if(localStorage.getItem('token') == null){
-    alert('Você precisa estar logado para acessar essa página')
-    window.location.href = '../html/signin.html'
-}
-
 let userLogado = JSON.parse(localStorage.getItem('userLogado')) 
 
 let logado = document.querySelector('#logado')
